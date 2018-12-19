efc294a6ecaa428dedf02811fa3b9196627e334b
Andrzej Senderecki <skidrzej@gmail.com>
2018-12-19 22:11:42 +0100

    Dodanie hamburger menu dla wersji mobile i desktop

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,13 @@ require('../scss/partials/_header.scss');
 require('../scss/partials/_phoneSection.scss');
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    const menuBtn = document.querySelector('.header-menu-icon');
+    const menuContent = document.querySelector('.header-menu-content');
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('header-menu-icon-active');
+        menuContent.classList.toggle('header-menu-content-invisible');
+    });
+
 });
