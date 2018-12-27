abf336a5126c3c38ec9d0ea651e1890658086704
Andrzej Senderecki <skidrzej@gmail.com>
2018-12-27 16:22:37 +0100

    Dodanie sekcji Category

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 require('../scss/partials/_main.scss');
 require('../scss/partials/_header.scss');
 require('../scss/partials/_phoneSection.scss');
+require('../scss/partials/_category.scss');
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -18,24 +19,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const $header = $('.header-bg');
     const $main = $('main');
     const $headerLogo = $('.header-logo');
+    const $headerContent = $('.header-content');
 
     $document.on('scroll', () => {
         const $docScroll = $document.scrollTop();
         const $headerHeight = $header.outerHeight();
         $header.css('opacity', 1 - $docScroll/$headerHeight);
         $headerLogo.css('opacity', 1 - $docScroll/$headerHeight);
+        $headerContent.css('opacity', 1 - $docScroll/$headerHeight);
     });
 
     // jQuery scroll to sections
 
-    const $aboutLink = $('#about');
+    const $phoneSection = $('.phone-section-container');
+    const $categorySection = $('.category-section-container');
 
-    $('#about').on('click', function() {
-        $('html').animate({
-            scrollTop: $main.offset().top
-        }, 1000);
+    $('#about').on('click', () => {
+
         menuBtn.classList.toggle('header-menu-icon-active');
         menuContent.classList.toggle('header-menu-content-invisible');
+
+        $('html').animate({
+            scrollTop: $phoneSection.offset().top
+        }, 1500);
+
+    });
+
+    $('#products').on('click', () => {
+
+        menuBtn.classList.toggle('header-menu-icon-active');
+        menuContent.classList.toggle('header-menu-content-invisible');
+
+        $('html').animate({
+            scrollTop: $categorySection.offset().top,
+        }, 2000);
     });
 
 });
